6deabf1ca378806f7d6440b531c8d4dbcfb224d5
Bianca Forbeci <biaformic@hotmail.com>
2018-03-06 15:11:46 -0300

    Incluindo novamente aula 1 no diretório

---
+++ Aula1_2802/Aula1_2802/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1_2802
+{
+    class Pessoa
+    {
+        public string Nome { get; set; }
+
+        public int PessoaID { get; set; }
+
+    }
+}
